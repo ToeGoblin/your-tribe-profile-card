db8bfff7dd6a7f1dd1fc5c01edbca77e1d528100
ToeGoblin <124787398+ToeGoblin@users.noreply.github.com>
2024-09-02 12:34:20 +0200

    Visitekaartje FDND Sjors Koopman

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') ///Hier maak je de classes van de code
+expandButton.addEventListener('click', expand) ///Hier classificeer wanneer de functie moet worden uitgevoerd
 
 function expand () {
-  document.body.classList.toggle('expand')
+  document.body.classList.toggle('expand') ///Hier zegt de class dat de functie "expand" moet worden uitgevoerd, wat verder gebeurd en wordt gedefinieerd in de css
 }
